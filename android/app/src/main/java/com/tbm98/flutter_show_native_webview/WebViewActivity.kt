a410805787a6d4354f86c40d752e2ecb34a833ca
tbm98 <tbm98.dev@gmail.com>
2020-09-22 17:16:04 +0700

    add webview & init home url

--- android/app/src/main/java/com/tbm98/flutter_show_native_webview/WebViewActivity.kt
+++ android/app/src/main/java/com/tbm98/flutter_show_native_webview/WebViewActivity.kt
@@ -3,10 +3,21 @@ package com.tbm98.flutter_show_native_webview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class WebViewActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
+        webView = findViewById(R.id.webview)
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+        webView.loadUrl("https://www.google.com/")
     }
 }
